647f4e70c0bf7c2023f48612c7b09b92d3daf0d2
jr.park <jr.park@equalsum.com>
2024-07-01 16:10:32 +0900

    React 소스 정리

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.js';
+import App from './App';
 
 
 describe('App', () => {
