fd5f6d50da7028445668d6ad0b8d3644f635a02f
jr.park <jr.park@equalsum.com>
2024-07-01 16:08:05 +0900

    빌드 관련 수정

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-
+import App from './App.js';
 
 
 describe('App', () => {
---
+++ src/setupTests.js
@@ -0,0 +1,8 @@
+// mock matchMedia
+window.matchMedia = window.matchMedia || function() {
+    return {
+        matches: false,
+        addListener: function() {},
+        removeListener: function() {}
+    };
+};
